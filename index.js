1e4316b5bdc2e66904d6e3719a40b3d1df13afb7
Joanna Huang <joannathhuang@gmail.com>
2019-07-22 22:53:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 // write your code below!
-function 
+function happyHolidays() {
+  return "Happy Holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `happy holidays, ${name}`
+}
+
+function happyHolidayTo(holiday, name) {
+  return `happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(days, holiday) {
+  return `it's ${days} days until ${holiday}!`
+}
